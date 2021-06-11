5f037ae08a882dc1847c1f3ac87a65bdf62b4b6b
Arcel <61158962+ArcelBrainly@users.noreply.github.com>
2021-06-11 21:55:24 +0530

    Game Code
    All of the game code

---
+++ game.py
@@ -0,0 +1,26 @@
+import pygame
+from pygame.locals import *
+
+pygame.init()
+
+screen_width = 1000
+screen_height = 1000
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('Platformer')
+
+sun_img = pygame.image.load('C:\\Users\\AMALSHAJI\\Documents\\Pygame\\sun.png')
+bg_img = pygame.image.load('C:\\Users\\AMALSHAJI\\Documents\\Pygame\\sky.img')
+run = True
+while run:
+
+    screen.blit(bg_img, (0, 0))
+    screen.blit(sun_img, (100, 100))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    pygame.display.update()
+
+pygame.quit()
